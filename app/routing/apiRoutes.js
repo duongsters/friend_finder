64827edf415b40fe93788f3a83453eb8be37f37f
Andrew Duong <duongsters@gmail.com>
2019-10-07 23:54:41 -0700

    .get method created to display app json values of friends index

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,1 +1,7 @@
 var friendArr = require("../data/friends.js");
+
+module.exports = function(app){
+    //a GET route method that displays JSON of all friends within the index
+    app.get('/api/friends', function(req,res){
+      res.json(friendArr);
+    });
