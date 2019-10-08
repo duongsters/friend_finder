5e3b94c514b22c385d503b523a58bf62b58adc11
Andrew Duong <duongsters@gmail.com>
2019-10-07 22:57:53 -0700

    implemented the routers that will direct to portservers for route.js files

--- server.js
+++ server.js
@@ -8,3 +8,8 @@ var PORT =  process.env.PORT || 8080;
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//Routers...will direct the port server to the route the apiRoutes & htmlRoutes files
+//These 2 route files will direct the server in how to respond to user's GET and REQUEST data
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlROutes")(app);
