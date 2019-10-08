1f37a470681e097199f5677fabecda2944321a05
Andrew Duong <duongsters@gmail.com>
2019-10-07 23:10:18 -0700

    added routing with the use of .get and .use

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,2 +1,14 @@
 //NPM Path Dependency
 var path = require('path');
+
+module.exports = function(app){ 
+    // survey page
+    app.get('/survey', function(req, res){
+    res.sendFile(path.join(__dirname + '/../public/survey.html'));
+    });
+    // home page
+    app.use(function(req, res){
+      res.sendFile(path.join(__dirname + '/../public/home.html'));
+    });
+    
+    };
