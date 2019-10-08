0f0658d53a208c56073bc0a8fc5a01b4e31e1967
Andrew Duong <duongsters@gmail.com>
2019-10-07 23:56:09 -0700

    .post method made to compare the survey results

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -5,3 +5,9 @@ module.exports = function(app){
     app.get('/api/friends', function(req,res){
       res.json(friendArr);
     });
+    app.post('/api/friends', function (req, res) {
+        // .post method to compare the scores of the friendArr index and user/admin's survey results
+        var newFriendScoreArray = req.body.scores;
+        var scoresArray = [];
+        var bestMatch = 0;
+        var count = 0;
