f5726ab31e4445694027a9e7e18168204a8d48da
Andrew Duong <duongsters@gmail.com>
2019-10-07 22:51:24 -0700

    started working on friends.js and added one example within the array index

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendArr = [
+    {
+        name: "Andrew Duong",
+        pic: "https://pbs.twimg.com/profile_images/1176299106634878976/8IsEszzI_400x400.jpg",
+        scores: [
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3",
+            "3"
+        ]
+    }
+];
